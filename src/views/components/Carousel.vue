5fba5f67e51e3104099a8af43173b808343918d3
Jafari Mchasa Akida <jakida@tucode.co.ke>
2023-08-08 11:21:50 +0300

    azure

--- src/views/components/Carousel.vue
+++ src/views/components/Carousel.vue
@@ -10,14 +10,12 @@
         </div>
         <div class="container py-0 pb-lg">
             <div class="row justify-content-between align-items-center">
-                <div class="col-lg-5 mb-5 mb-lg-0">
-                    <h1 class="text-white font-weight-light">Bootstrap carousel</h1>
-                    <p class="lead text-white mt-4">Argon Design System comes with four pre-built pages to help you get
-                        started faster. You can change the text and images and you're good to go.</p>
-                    <a href="https://demos.creative-tim.com/argon-design-system/docs/components/alerts.html"
-                       class="btn btn-white mt-4">See all components</a>
+                <div class="col-lg-4 mb-5 mb-lg-0">
+                    <h1 class="text-white font-weight-light">About Us</h1>
+                    <a href="#"
+                       class="btn btn-white mt-4">Contact Us</a>
                 </div>
-                <div class="col-lg-6 mb-lg-auto">
+                <div class="col-lg-8 mb-lg-auto">
                     <div class="rounded shadow-lg overflow-hidden transform-perspective-right">
                         <b-carousel id="carousel1"
                                     controls
