7ee1bd9e3d75babfeabb29fdb56bc484042b5b50
Jafari Mchasa Akida <jakida@tucode.co.ke>
2023-08-08 11:46:54 +0300

    azure

--- src/views/components/Carousel.vue
+++ src/views/components/Carousel.vue
@@ -12,17 +12,35 @@
             <div class="row justify-content-between align-items-center">
                 <div class="col-lg-4 mb-5 mb-lg-0">
                     <h1 class="text-white font-weight-light">About Us</h1>
-                    <a href="#"
-                       class="btn btn-white mt-4">Contact Us</a>
+                    <a href="#" class="btn btn-white mt-4">Contact Us</a>
                 </div>
-                <div class="col-lg-8 mb-lg-auto" style="height: 500px;">
+                <div class="col-lg-8 mb-lg-auto">
                     <div class="rounded shadow-lg overflow-hidden transform-perspective-right">
-                        <b-carousel id="carousel1"
-                                    controls
-                                    indicators>
+                        <b-carousel id="carousel1" controls indicators>
                             <!-- Text slides with image -->
                             <b-carousel-slide img-src="img/theme/img-1-1200x1000.jpg"></b-carousel-slide>
                             <b-carousel-slide img-src="img/theme/img-2-1200x1000.jpg"></b-carousel-slide>
+                            <b-carousel-slide>
+                                caption="TuCode CDL Phase 1"
+                                text="Have An Idea? You Wonder How You Can Get It Done And Be Implemented Digitaly!"
+                                style="height: 562px; background-color: transparent;">
+                                <div class="col-12">
+                                    <card class="border-0" hover shadow body-classes="py-5">
+                                        <icon name="ni ni-check-bold" type="primary" rounded class="mb-4">
+                                        </icon>
+                                        <h6 class="text-primary text-uppercase">Our Three Cs</h6>
+                                        <div>
+                                            <badge type="primary" rounded>Contact</badge>
+                                            <badge type="primary" rounded>Consult</badge>
+                                            <badge type="primary" rounded>Command</badge>
+                                        </div>
+                                        <base-button tag="a" href="#" type="primary" class="mt-4">
+                                            Get In Touch
+                                        </base-button>
+                                    </card>
+                                </div>
+
+                            </b-carousel-slide>
                         </b-carousel>
                     </div>
                 </div>
@@ -35,11 +53,10 @@ import { BCarousel } from "bootstrap-vue/esm/components/carousel/carousel";
 import { BCarouselSlide } from "bootstrap-vue/esm/components/carousel/carousel-slide";
 
 export default {
-  components: {
-    BCarousel,
-    BCarouselSlide
-  }
+    components: {
+        BCarousel,
+        BCarouselSlide
+    }
 };
 </script>
-<style>
-</style>
+<style></style>
